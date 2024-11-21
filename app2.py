6642557b917ca8d42cf7e1b6d190767a8776a3e9
NGCC-42 <nick@clubcannon.com>
2024-11-21 07:39:42 -0800

    Commented out plotly import

--- app2.py
+++ app2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as px
+#import plotly.express as px
 from PIL import Image
 import numpy as np
 from collections import ChainMap, defaultdict
