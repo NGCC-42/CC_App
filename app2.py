c682730c993d9932305dbb7e035ef5d7b3297fd7
NGCC-42 <nick@clubcannon.com>
2025-01-07 13:31:32 -0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -736,7 +736,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 with st.sidebar:
-    task_choice = option_menu(None, ["Dashboard", "Product Reports",  "Customer Details", "Shipping Reports", "Quote Reports", "Leaderboards"], 
+    task_choice = option_menu(None, ["Dashboard", "Product Reports",  "Customer Details", "Leaderboards", "Shipping Reports", "Quote Reports"], 
         icons=['house', 'projector', 'person-circle', 'box-seam', 'shadows', 'trophy'], 
         menu_icon="cast", default_index=0, orientation="vertical",
         styles={
