f82b08149f4badde6032522c9f3c4a84eabff93a
NGCC-42 <nick@clubcannon.com>
2025-01-08 13:24:11 -0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -4400,12 +4400,12 @@ if task_choice == 'Leaderboards':
     cola, colb, colc = st.columns([.25, .5, .25])
 
     colb.header('Customer Leaderboards')	
-
-    spend_year = colb.selectbox('Choose Year', ['2024', '2023'])
-    ranking_number = colb.selectbox('Choose Leaderboard Length', [5, 10, 15, 20, 25, 50])
+    with colb:
+        spend_year = st.selectbox('Choose Year', ['2024', '2023'])
+        ranking_number = st.selectbox('Choose Leaderboard Length', [5, 10, 15, 20, 25, 50])
     
     for cust in unique_customer_list:
-		cust_spend_dict_2023[cust] = 0
+        cust_spend_dict_2023[cust] = 0
 		cust_spend_dict_2024[cust] = 0
 	
 	idx = 0
