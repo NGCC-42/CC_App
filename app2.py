2ee72d25bc85bd96c9490c884ef825fb357cb14f
NGCC-42 <nick@clubcannon.com>
2025-01-31 11:46:26 -0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -3235,8 +3235,8 @@ def plot_annual_comparison(x, year_select, col1, line_width=4.5, fig_width=12, f
     fig.patch.set_facecolor('#000000')
 
     # Customize tick labels for responsiveness
-    ax.tick_params(axis='x', labelsize=40, colors='white')
-    ax.tick_params(axis='y', labelsize=40, colors='white')
+    ax.tick_params(axis='x', labelsize=22, colors='white')
+    ax.tick_params(axis='y', labelsize=22, colors='white')
 
     # Set dynamic y-ticks based on data range
     #all_y_values = [globals().get(f"y{year}", []) for year in selected_years if globals().get(f"y{year}") is not None]
@@ -3250,7 +3250,7 @@ def plot_annual_comparison(x, year_select, col1, line_width=4.5, fig_width=12, f
     plt.tick_params(axis='y', colors='white')
     # Customize legend
     #ax.legend(fontsize=16, loc="upper right", frameon=False)
-    fig.legend(fontsize=40)
+    fig.legend(fontsize=22)
     # Ensure proper figure scaling for Streamlit
     col1.pyplot(fig, use_container_width=True)
 
