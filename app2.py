72072df363909a5727ba4efccf83e24f58955368
NGCC-42 <nick@clubcannon.com>
2025-02-04 09:34:17 -0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -222,6 +222,7 @@ def fix_names(df):
     df.replace('Yang Gao', 'Nebula NY', inplace=True)
     df.replace('Adam Stipe', 'Special Event Services (SES)', inplace=True)
     df.replace('Michael Brammer', 'Special Event Services (SES)', inplace=True)
+    df.replace('ffp effects inc c/o Third Encore', 'FFP FX', inplace=True)
 
     return df
 
