00dfc01f809bad13bbba159227b358a27877eeb8
NGCC-42 <nick@clubcannon.com>
2025-02-05 13:14:22 -0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -234,6 +234,7 @@ def fix_names(df):
     df.replace('MICHAEL MELICE', 'Michael Melice', inplace=True)
     df.replace('Michael Brammer / Special Event Services', 'Special Event Services (SES)', inplace=True)
     df.replace('Dios Vazquez ', 'Dios Vazquez', inplace=True)
+    df.replace('Brilliant Stages Ltd T/A TAIT', 'Brilliant Stages', inplace=True)
     
     return df
 
