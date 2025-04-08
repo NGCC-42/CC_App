91e97ee4cacce5bf9c1a20b6def1d24f55e66469
NGCC-42 <nick@clubcannon.com>
2025-04-08 15:24:26 -0700

    Update extract_acc_data() to fix CGA issue

--- app2.py
+++ app2.py
@@ -977,7 +977,7 @@ bom_cost_jet = {'Pro Jet': 290.86, 'Quad Jet': 641.43, 'Micro Jet': 243.57, 'Cry
 bom_cost_control = {'The Button': 141.07, 'Shostarter': 339.42, 'Shomaster': 667.12}
 bom_cost_hh = {'8FT - No Case': 143.62, '8FT - Travel Case': 219.06, '15FT - No Case': 153.84, '15FT - Travel Case': 231.01}
 bom_cost_hose = {'2FT MFD': 20.08, '3.5FT MFD': 22.50, '5FT MFD': 24.25, '5FT STD': 31.94, '5FT DSY': 31.84, '5FT EXT': 33.24, '8FT STD': 32.42, '8FT DSY': 34.52, '8FT EXT': 34.82, '15FT STD': 43.55, '15FT DSY': 46.47, '15FT EXT': 46.77, '25FT STD': 59.22, '25FT DSY': 61.87, '25FT EXT': 62.17, '35FT STD': 79.22, '35FT DSY': 81.32, '35FT EXT': 81.62, '50FT STD': 103.57, '50FT EXT': 105.97, '100FT STD': 183.39}
-bom_cost_acc = {'CC-AC-CCL': 29.17, 'CC-AC-CTS': 6.70, 'CC-F-DCHA': 7.15, 'CC-F-HEA': 6.86, 'CC-AC-RAA': 11.94, 'CC-AC-4PM': 48.12, 'CC-F-MFDCGAJIC': 7.83, ' CC-AC-CGAJIC-SET': 5.16, 'CC-CTC-20': 10.92, 'CC-CTC-50': 19.36, 'CC-AC-TC': 89.46, 'CC-VV-KIT': 29.28, 
+bom_cost_acc = {'CC-AC-CCL': 29.17, 'CC-AC-CTS': 6.70, 'CC-F-DCHA': 7.15, 'CC-F-HEA': 6.86, 'CC-AC-RAA': 11.94, 'CC-AC-4PM': 48.12, 'CC-F-MFDCGAJIC': 7.83, ' CC-AC-CGAJIC-SET': 5.16, 'CC-AC-CGAJIC-SET - 1': 5.16, 'CC-CTC-20': 10.92, 'CC-CTC-50': 19.36, 'CC-AC-TC': 89.46, 'CC-VV-KIT': 29.28, 
                 'CC-RC-2430': 847, 'CC-AC-LA2': 248.10, 'CC-SW-05': 157.24, 'CC-NPTC-06-STD': 10.99, 'CC-NPTC-10-DSY': 18.90, 'CC-NPTC-15-DSY': 27.08, 'CC-NPTC-25-DSY': 39.37}
 bom_cost_mfx = {'MagicFX Commander': 355.73, 'Magic FX Smoke Bubble Blaster': 3328.63, 'MagicFX ARM SFX SAFETY TERMINATOR': 12.50, 'MagicFX Device Updater': 38.37, 'MagicFX PSYCO2JET': 1158.63, 'MagicFX Red Button': 61.23, 'MagicFX Replacement Keys': 7.27, 
                 'MagicFX SFX Safety ARM Controller': 616.13, 'MagicFX SPARXTAR': 1623.63, 'MagicFX Sparxtar powder': 19.84, 'MagicFX StadiumBlaster': 2893.56, 'MagicFX StadiumBlower': 2858.90, 'MagicFX StadiumShot III': 2321.13, 'MagicFX SuperBlaster II': 1468.63, 
@@ -1729,6 +1729,7 @@ def extract_acc_data(df):
         'CC-AC-4PM': 9,
         'CC-F-MFDCGAJIC': 14,
         ' CC-AC-CGAJIC-SET': 17,  # note the leading space if that is intentional
+        'CC-AC-CGAJIC-SET - 1: 20,
         'CC-CTC-20': 9,
         'CC-CTC-50': 9,
         'CC-AC-TC': 8,
@@ -1806,6 +1807,15 @@ def extract_acc_data(df):
                 grp_special = df_rc.loc[mask_special].groupby("month")["quantity"].sum()
                 for month, qty in grp_special.items():
                     results[yr][month][rc_key][idx_to_update] = qty
+
+    # Combine CGAJIC set sku totals 
+    for yr in target_years:
+        for month in months_x:
+            results[yr][month][' CC-AC-CGAJIC-SET'][0] += results[yr][month]['CC-AC-CGAJIC-SET - 1'][0]
+            results[yr][month][' CC-AC-CGAJIC-SET'][1] += results[yr][month]['CC-AC-CGAJIC-SET - 1'][1]
+            
+            # Remove revision of CGAJIC set
+            del results[yr][month]['CC-AC-CGAJIC-SET - 1']
                     
     # Return dictionaries for each year: 2023, 2024, 2025.
     return results[2023], results[2024], results[2025]
